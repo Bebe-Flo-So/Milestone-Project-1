69bfdbebc0df6ade68bbee313ebe1eab60914cda
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-04-14 14:52:26 -0400

    updated images

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-let 
-const btn= document.getElementById('button')
-btn.addEventListener('click',()=>{
-    console.log('button was clicked')
+document.addEventListener('DOMContentLoaded', () => {
+
+ const cardsList = [
+    {
+        name: 'bee',
+        image: 'assets/bee.jpg'
+    },
+    {
+        name: 'bee',
+        image: 'assets/bee.jpg'
+    }
+    {
+        name: 'butterfly',
+        image: 'assets/butterfly.jpg'
+    }
+    {
+        name: 'butterfly',
+        image: 'assets/butterfly.jpg'
+    }
+    {
+        name: 'flower',
+        image: 'assets/flower.jpg'
+    }
+    {
+        name: 'flower',
+        image: 'assets/flower.jpg'
+    }
+    {
+        name: 'hummingbird',
+        image: 'assets/hummingbird.jpg'
+    }
+    {
+        name: 'hummingbird',
+        image: 'assets/hummingbird.jpg'
+    }
+
+    {
+        name: 'ladybug',
+        image: 'assets/ladybug.jpg'
+    }
+    {
+        name: 'ladybug',
+        image: 'assets/ladybug.jpg'
+    }
+ ]   
+
+ 
 })
